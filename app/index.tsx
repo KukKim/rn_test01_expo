e5e16ff8b3a4dc5c143a1e361037496d2d101545
Dongkuk Kim <dongkukkim@Dongkukui-MacBookPro.local>
2025-07-28 18:15:55 +0900

    feat: add auth screen
    feat: add auth screen2

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,35 @@
-import { Link, Redirect } from "expo-router";
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
+// import { Link, Redirect } from "expo-router";
+// import { useEffect } from "react";
+// import { Text, View } from "react-native";
 
 export default function Index() {
-  const login = true;
+  const login = false;
+
+  // useEffect(() => {
+  //   if (login) {
+  //     return <Redirect href="/(tabs)" />;
+  //   } else {
+  //     return <Redirect href="/login" />;
+  //   }
+  // }, [login]);
+
+  // return (
+  //   <View
+  //     style={{
+  //       flex: 1,
+  //       justifyContent: "center",
+  //       alignItems: "center",
+  //     }}
+  //   >
+  //     <Text>index</Text>
+  //     <Link href="/settings">View details</Link>
+  //   </View>
+  // );
 
   if (login) {
     return <Redirect href="/(tabs)" />;
+  } else {
+    return <Redirect href="/login" />;
   }
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>index</Text>
-      <Link href="/settings">View details</Link>
-    </View>
-  );
 }
--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,9 +1,61 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import CommonButton from "../../components/button/commonButton";
 
 export default function LoginScreen() {
+  const [id, setId] = React.useState("");
+  const [pwd, setPwd] = React.useState("");
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/images/testLogo.png")}
+        />
+      </View>
+      <View style={styles.loginContainer}>
+        <View style={styles.txtInputContainer}>
+          <TextInput
+            style={styles.txtInput}
+            placeholder="Email"
+            onChangeText={setId}
+            value={id}
+          />
+        </View>
+        <View style={styles.txtInputContainer}>
+          <TextInput
+            placeholder="Password"
+            style={styles.txtInput}
+            onChangeText={setPwd}
+            value={pwd}
+          />
+        </View>
+        <CommonButton
+          onPress={() => {
+            console.log("Login");
+          }}
+        >
+          <Text>Login</Text>
+        </CommonButton>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.socialLoginBtn}>
+          <Text>Social Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialLoginBtn}>
+          <Text>Social Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialLoginBtn}>
+          <Text>Social Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,6 +64,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+  logoContainer: {
     alignItems: "center",
+    padding: 5,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  loginContainer: {},
+  txtInputContainer: {
+    padding: 5,
+    margin: 5,
+  },
+  txtInput: {
+    paddingBottom: 5,
+    borderBottomWidth: 1,
+  },
+  socialLoginBtn: {
+    margin: 5,
+    padding: 5,
+    backgroundColor: "green",
   },
 });
